10da4919c2fde67980b3b457b27a9a61801067c7
Da Teng <dateng2016@gmail.com>
2026-09-21 17:15:47 -0400

    no new commnet line

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,14 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Don't continue comments when starting a new line.
+-- `r` = auto-insert comment leader after hitting <Enter> in insert mode
+-- `o` = auto-insert comment leader after hitting `o`/`O` in normal mode
+-- Done in a FileType autocmd because ftplugins set formatoptions per-buffer.
+vim.api.nvim_create_autocmd("FileType", {
+  group = vim.api.nvim_create_augroup("no_comment_continuation", { clear = true }),
+  callback = function()
+    vim.opt_local.formatoptions:remove({ "r", "o" })
+  end,
+})
